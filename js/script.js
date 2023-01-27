fe0c661f2cf4fc3f28a245918b4bed33dec2761c
Angelika Krygier <angelika.krygier96@gmail.com>
2023-01-27 09:29:44 +0100

    Tidying up of the JS code

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
-let buttonChangeFont = document.querySelector(".buttonChangeFont")
-let body = document.querySelector(".body")
-let buttonThemeName = document.querySelector(".js-colorName")
+{
+    const welcome = () => {
+        console.log("Witam Was serdecznie!")
+    }
 
-buttonChangeFont.addEventListener("click", () => {
-    body.classList.toggle("body__font");
-    buttonThemeName.innerText = body.classList.contains("body__font") ? "czarnym" : "niebieskim";
-})
+    const toggleFont = () => {
+        const body = document.querySelector(".body");
+        const buttonThemeName = document.querySelector(".js-colorName")
+
+        body.classList.toggle("body__font");
+        buttonThemeName.innerText = body.classList.contains("body__font") ? "czarnym" : "niebieskim"
+    };
+
+    const init = () => {
+        const buttonChangeFont = document.querySelector(".buttonChangeFont")
+        buttonChangeFont.addEventListener("click", toggleFont)
+    };
+
+    welcome();
+    init()
+
+};
